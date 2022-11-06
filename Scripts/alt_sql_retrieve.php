f5de4711a7a6332192d4a120d82e6bfda39a4840
os-enjoyer1 <111944396+os-enjoyer1@users.noreply.github.com>
2022-11-06 19:09:26 +0800

    Removed if-else statements
    The reason for this is that such a method does not scale that well if someone wants to add more food types. You'll then have to key in yet another if-else statement. IMO it seems better to store such a thing in an array instead.

--- Scripts/alt_sql_retrieve.php
+++ Scripts/alt_sql_retrieve.php
@@ -9,24 +9,15 @@
     $Q2 = $_GET['Q2'];
     $Q3 = $_GET['Q3'];
     $Q4 = $_GET['Q4'];
-    if($foodType == "pizza"){
-        $sql = 'select Piz_Result,Piz_Description from pizza where Piz_Meat=:Q1 and Piz_Hated=:Q2 and Piz_Cheese=:Q3 and Piz_Second=:Q4';
-    }
-    elseif($foodType == "burger"){
-        $sql = 'select Bur_Result,Bur_Description from burger where Bur_Bun=:Q1 and Bur_Patty=:Q2 and Bur_Hated=:Q3 and Bur_Cheese=:Q4';
-    }
-    elseif($foodType == "pasta"){
-        $sql = 'select Pas_Result,Pas_Description from pasta where Pas_Type=:Q1 and Pas_Meat=:Q2 and Pas_Sauce=:Q3 and Pas_Topping=:Q4';
-    }
-    elseif($foodType == "sandwich"){
-        $sql = 'select San_Result,San_Description from sandwich where San_Bread=:Q1 and San_Meat=:Q2 and San_Hated=:Q3 and San_Cooking=:Q4';
-    }
-    elseif($foodType == "steak"){
-        $sql = 'select Ste_Result,Ste_Description from steak where Ste_Rarity=:Q1 and Ste_Wine=:Q2 and Ste_Sauce=:Q3 and Ste_Hated=:Q4';
-    }
-    elseif($foodType == "wings"){
-        $sql = 'select Win_Result,Win_Description from wings where Win_Flavour=:Q1 and Win_Dip=:Q2 and Win_Cooking=:Q3 and Win_Hated=:Q4';
-    }
+
+    $sql_arr = array('pizza' => 'select Piz_Result,Piz_Description from pizza where Piz_Meat=:Q1 and Piz_Hated=:Q2 and Piz_Cheese=:Q3 and Piz_Second=:Q4', 
+                     'burger' => 'select Bur_Result,Bur_Description from burger where Bur_Bun=:Q1 and Bur_Patty=:Q2 and Bur_Hated=:Q3 and Bur_Cheese=:Q4',
+                     'pasta' => 'select Pas_Result,Pas_Description from pasta where Pas_Type=:Q1 and Pas_Meat=:Q2 and Pas_Sauce=:Q3 and Pas_Topping=:Q4',
+                     'sandwich' => 'select San_Result,San_Description from sandwich where San_Bread=:Q1 and San_Meat=:Q2 and San_Hated=:Q3 and San_Cooking=:Q4',
+                     'steak' => 'select Ste_Result,Ste_Description from steak where Ste_Rarity=:Q1 and Ste_Wine=:Q2 and Ste_Sauce=:Q3 and Ste_Hated=:Q4',
+                     'wings' => 'select Win_Result,Win_Description from wings where Win_Flavour=:Q1 and Win_Dip=:Q2 and Win_Cooking=:Q3 and Win_Hated=:Q4');
+    
+    $sql = $sql_arr[$foodType];
     $stmt = $pdo->prepare($sql);
     $stmt->bindParam(':Q1', $Q1, PDO::PARAM_STR);
     $stmt->bindParam(':Q2', $Q2, PDO::PARAM_STR);
