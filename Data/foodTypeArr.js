43abf0ebba736a869d1abead24b8c782deb1223f
os-enjoyer1 <111944396+os-enjoyer1@users.noreply.github.com>
2022-11-06 20:20:13 +0800

    Data for categories.js now stored in foodTypeArr.js

---
+++ Data/foodTypeArr.js
@@ -0,0 +1,12 @@
+food_cat_arr = 
+    {
+        0: "burger",
+        1: "pasta",
+        2: "pizza",
+        3: "sandwich",
+        4: "steak",
+        5: "wings",
+    }
+
+sessionStorage.setItem("food_cat_arr", JSON.stringify(food_cat_arr));
+    
