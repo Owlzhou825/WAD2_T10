59ab6727bcc6190bfe48e7f6f974925a7d648469
os-enjoyer1 <111944396+os-enjoyer1@users.noreply.github.com>
2022-11-06 18:54:47 +0800

    Added _foodTypeOptions.js
    This is mostly done to make the code more readable. _foodTypeOptions.js also allows for updating of question options without touching _universalFood.js.
    
    Other html files have been adapted to account for usage of _foodTypeOptions.js.

--- Scripts/_universalFood.js
+++ Scripts/_universalFood.js
@@ -16,44 +16,6 @@ function restart() {
             quiz_result:"",
             quiz_desc:"",
             quiz_image:"",
-            options_for_food_type:{
-              burger: {
-                Q1:["Plain", "Sesame", "Charcoal", "Sourdough"],
-                Q2:["Beef", "Chicken", "Fish", "Pork"],
-                Q3:["Mushroom", "Tomato", "Lettuce", "Onion"],
-                Q4:["American", "Cheddar", "Brie", "None"]
-              },
-              pasta: {
-                Q1:["Spaghetti", "Fusilli", "Linguine", "Fettuccine"],
-                Q2:["Beef", "Chicken", "Seafood", "None"],
-                Q3:["White Cream", "Tomato", "Pesto", "None"],
-                Q4:["Herbs", "Pepper", "Garlic", "None"]
-              },
-              pizza: {
-                Q1:["Pepperoni", "Beef", "Ham", "None"],
-                Q2:["Pineapple", "Olive", "Bellpepper", "Chilli"],
-                Q3:["Mozzarella", "Parmesan", "Cheddar", "Gouda"],
-                Q4:["Chicken", "Bacon", "Pork", "None"]
-              },
-              sandwich: {
-                Q1: ["Baguette", "White Bread", "Sourdough", "Wholemeal"],
-                Q2: ["Chicken", "Beef", "Pork", "Fish"],
-                Q3: ["Tomato", "Lettuce", "Cheese", "Egg"],
-                Q4: ["Grilled", "Pressed", "Cold", "Oven Bake"]
-              },
-              steak: {
-                Q1: ["Rare", "Medium Rare", "Medium", "Well Done"],
-                Q2: ["Cabernet", "Syrah", "Merlot", "None"],
-                Q3: ["Peppercorn", "Mushroom", "Soy Butter", "Red Wine"],
-                Q4: ["Fries", "Vegetable", "Potato", "None"]
-              },
-              wings: {
-                Q1: ["Sweet", "Spicy", "Sour", "Plain"],
-                Q2: ["Mayo", "Mustard", "Chilli", "Tomato"],
-                Q3: ["Pan Fry", "Deep Fry", "Oven Bake", "Air Fry"],
-                Q4: ["Garlic", "Onion", "Sesame", "Pepper"]
-              }
-            },
             question_option:{
               Q1:
                 [{key:"Q1_1", value:""}, 
@@ -80,11 +42,12 @@ function restart() {
       },methods:{
         initialise(){
           this.foodType = sessionStorage.getItem("food_cat")
+          let options_for_food_type = JSON.parse(sessionStorage.getItem("options_for_food_type"))
           for (question in this.question_option) {
             question_arr = this.question_option[question]
             for (idx in question_arr) {
               option = question_arr[idx]
-              option.value = this.options_for_food_type[this.foodType][question][idx]
+              option.value = options_for_food_type[this.foodType][question][idx]
             }
           }
         },
